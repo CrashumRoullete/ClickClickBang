dfde62b8bba680186c28340f94a22601f6a9aefe
Tyler Chong <tylrchong@gmail.com>
2016-11-11 18:07:21 -1000

    Sockets

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 var express = require('express')
-var app = express()
-
-app.connect(app.get('port'), function() {
-  console.log('test')
-})
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello from Node Knockout 2016!')
+app.get('/', function(req, res) {
+  res.send('Hello from Node Knockout 2016!')
 })
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+io.on('connection', function (socket) {
+  console.log('test')
+  //our other events...
+});
