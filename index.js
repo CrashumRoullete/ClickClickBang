a6f7d39603a63a553749b1ce014d13a2c20548a9
Tyler Chong <tylrchong@gmail.com>
2016-11-11 21:16:02 -1000

    Pushing Socket ID into database

--- index.js
+++ index.js
@@ -2,45 +2,42 @@ var express = require('express')
 var app = express()
 var server = require('http').Server(app)
 var io = require('socket.io')(server)
+io.origins('*:*')
+// var cors = require('cors')
 var config = require('./config.js')
-
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-// app.get('/', function(request, response) {
-//   response.send('Hello from Node Knockout 2016!')
-// })
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
-
 const MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect('mongodb://ds139937.mlab.com:39937/clickclickbang', (err, db) => {
-    if (err) console.dir(err)
-    if (!err) {
-      console.log('successfully connected to the database')
-      db.authenticate(config.username, config.password, (err, res) => {
-        if (err) return console.log(err)
-        console.log('Successfully authenticated into the database')
-        const collection = db.collection('game')
-        collection.insertOne(
-          {
-            id: 'id',
-            socket: 'socket',
-          },
-          (err, result) => {
-            if (err) console.log(err)
-          })
-        db.close()
-      })
-    }
-  })
+const PORT = process.env.PORT || 5000;
+// app.use(cors())
+app.use(express.static(__dirname + '/public'))
+
+server.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
 
 const sockets = []
 
 io.on('connection', function (socket) {
   sockets.push(socket)
-  socket.emit('news', { hello: 'world' });
-});
+  socket.on('join room', function (data) {
+    console.log(data)
+    MongoClient.connect('mongodb://ds139937.mlab.com:39937/clickclickbang', (err, db) => {
+        if (err) console.log(err)
+        if (!err) {
+          console.log('successfully connected to the database')
+          db.authenticate(config.username, config.password, (err, res) => {
+            if (err) return console.log(err)
+            console.log('Successfully authenticated into the database')
+            const collection = db.collection('game')
+            collection.insertOne(
+              {
+                id: data.id,
+              },
+              (err, result) => {
+                if (err) console.log(err)
+              })
+            db.close()
+          })
+        }
+      })
+  })
+
+})
