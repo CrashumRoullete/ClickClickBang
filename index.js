e2b2063a6e4465b72052446865ab18889b07fac5
Tyler Chong <tylrchong@gmail.com>
2016-11-11 23:48:13 -1000

    Test

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
 
+app.get('/', (req, res) => {
+  res.send(PORT)
+})
+
 app.post('/', (req, res) => {
   console.log(req.body)
 })
