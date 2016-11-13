e4c3cd72c9ef46f09067c4a08d2d744115343a1a
Tyler Chong <tylrchong@gmail.com>
2016-11-13 01:35:54 -1000

    first try

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -29,7 +29,7 @@ class Game extends React.Component{
     setInterval(() => {
       const req = new XMLHttpRequest();
       req.addEventListener('load', this.onMongoData);
-      req.open('GET', 'http://localhost:5000/data');
+      req.open('GET', 'http://clickclickbang.2016.nodeknockout.com/data');
       req.send();
     }, 1000)
 
@@ -37,7 +37,7 @@ class Game extends React.Component{
 
     let that = this;
 
-    var socket = io('http://clickclickbang.2016.nodeknockout.com/')
+    var socket = io('http://localhost:5000')
     socket.on('join room', function(param) {
       that.setState({ player1: socket.id, player2: param.opponentId });
       that.setState({ gameOn: true });
